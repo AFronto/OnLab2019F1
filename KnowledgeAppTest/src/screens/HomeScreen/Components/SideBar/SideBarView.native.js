0c9cf9a7387d2fd271709d14f13b1564c5ad85ae
FrontoA <andrasfronto@gmail.com>
2019-09-09 14:02:26 +0200

    Stable works on mobile as well

--- KnowledgeAppTest/src/screens/HomeScreen/Components/SideBar/SideBarView.native.js
+++ KnowledgeAppTest/src/screens/HomeScreen/Components/SideBar/SideBarView.native.js
@@ -1,30 +1,42 @@
 import React from "react";
 import { Content, Text, List, Button, Icon, ListItem } from "native-base";
-import commonStyles from '../commonStyles';
+import commonStyles from "../../../_common/commonStyles";
 
 export default class SideBarView extends React.Component {
-    render() {
-        return (
-            <Content style={{ backgroundColor: '#131726' }}>
-                <List>
-                    <Button transparent iconLeft onPress={() => this.props.redirectTo("/profile")}>
-                        <Icon name="md-contact" style={commonStyles.menuIcon} />
-                        <Text style={commonStyles.menuText}>Profile</Text>
-                    </Button>
-                    <Button transparent iconLeft onPress={() => this.props.redirectTo("/skills")}>
-                        <Icon name="md-cloud" style={commonStyles.menuIcon} />
-                        <Text style={commonStyles.menuText}>Skills</Text>
-                    </Button>
-                    <Button transparent iconLeft onPress={() => this.props.redirectTo("/")}>
-                        <Icon name="md-chatbubbles" style={commonStyles.menuIcon} />
-                        <Text style={commonStyles.menuText}>Threads</Text>
-                    </Button>
-                    <Button transparent iconLeft onPress={this.props.deleteJWT}>
-                        <Icon name="md-exit" style={commonStyles.menuIcon} />
-                        <Text style={commonStyles.menuText}>Log Out</Text>
-                    </Button>
-                </List>
-            </Content>
-        );
-    }
+  render() {
+    return (
+      <Content style={{ backgroundColor: "#131726" }}>
+        <List>
+          <Button
+            transparent
+            iconLeft
+            onPress={() => this.props.redirectTo("/profile")}
+          >
+            <Icon name="md-contact" style={commonStyles.menuIcon} />
+            <Text style={commonStyles.menuText}>Profile</Text>
+          </Button>
+          <Button
+            transparent
+            iconLeft
+            onPress={() => this.props.redirectTo("/skills")}
+          >
+            <Icon name="md-cloud" style={commonStyles.menuIcon} />
+            <Text style={commonStyles.menuText}>Skills</Text>
+          </Button>
+          <Button
+            transparent
+            iconLeft
+            onPress={() => this.props.redirectTo("/")}
+          >
+            <Icon name="md-chatbubbles" style={commonStyles.menuIcon} />
+            <Text style={commonStyles.menuText}>Threads</Text>
+          </Button>
+          <Button transparent iconLeft onPress={this.props.deleteJWT}>
+            <Icon name="md-exit" style={commonStyles.menuIcon} />
+            <Text style={commonStyles.menuText}>Log Out</Text>
+          </Button>
+        </List>
+      </Content>
+    );
+  }
 }
--- KnowledgeAppTest/src/screens/SkillsScreen/SkillView.native.js
+++ KnowledgeAppTest/src/screens/SkillsScreen/SkillView.native.js
@@ -1,100 +1,113 @@
-import React, { Component } from 'react';
-import commonStyles from '../components/commonStyles';
-import { Text, View, List, ListItem, Button, Icon, Fab} from "native-base";
-import { ListView, RefreshControl} from 'react-native';
+import React, { Component } from "react";
+import { Text, View, List, ListItem, Button, Icon, Fab } from "native-base";
+import { ListView, RefreshControl } from "react-native";
+import commonStyles from "../_common/commonStyles";
 
 export default class SkillView extends Component {
-    constructor(props) {
-        super(props);
-        this.ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
-        this.state = {
-            loading: true,
-            error: '',
-            skills: ""
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+    this.state = {
+      loading: true,
+      error: "",
+      skills: ""
+    };
+  }
 
-    deleteRow(secId, rowId, rowMap) {
-        rowMap[`${secId}${rowId}`].props.closeRow();
-        const newData = [...this.state.skills];
-        var id = newData[rowId].id;
-        newData.splice(rowId, 1);
-        this.setState({ skills: newData });
-        this.props.deleteSkill(id);
-    }
+  deleteRow(secId, rowId, rowMap) {
+    rowMap[`${secId}${rowId}`].props.closeRow();
+    const newData = [...this.state.skills];
+    var id = newData[rowId].id;
+    newData.splice(rowId, 1);
+    this.setState({ skills: newData });
+    this.props.deleteSkill(id);
+  }
 
-    addSkillPressed(secId, rowId, rowMap) {
-        rowMap[`${secId}${rowId}`].props.closeRow();
-        const newData = [...this.state.skills];
-        var id = newData[rowId].id;
-        newData[rowId].userKnows = true;
-        this.setState({ skills: newData });
-        this.props.addSkillToMe(id);
-    }
+  addSkillPressed(secId, rowId, rowMap) {
+    rowMap[`${secId}${rowId}`].props.closeRow();
+    const newData = [...this.state.skills];
+    var id = newData[rowId].id;
+    newData[rowId].userKnows = true;
+    this.setState({ skills: newData });
+    this.props.addSkillToMe(id);
+  }
 
-    removeSkillPressed(secId, rowId, rowMap) {
-        rowMap[`${secId}${rowId}`].props.closeRow();
-        const newData = [...this.state.skills];
-        var id = newData[rowId].id;
-        newData[rowId].userKnows = false;
-        this.setState({ skills: newData });
-        this.props.removeSkillFromMe(id);
-    }
+  removeSkillPressed(secId, rowId, rowMap) {
+    rowMap[`${secId}${rowId}`].props.closeRow();
+    const newData = [...this.state.skills];
+    var id = newData[rowId].id;
+    newData[rowId].userKnows = false;
+    this.setState({ skills: newData });
+    this.props.removeSkillFromMe(id);
+  }
 
-    _onRefresh = () => {
-        this.props.loadSkills();
-    }
+  _onRefresh = () => {
+    this.props.loadSkills();
+  };
 
-    render() {
-        return (
-                <View style={{ flex: 1 }}>
-                    <Text style={commonStyles.errorTextStyle}>
-                        {this.state.error}
-                    </Text>
-                    <List
-                        refreshControl={<RefreshControl
-                                            refreshing={this.state.loading}
-                                            onRefresh={this._onRefresh}
-                                            progressBackgroundColor={"#5cb85c"}
-                                            colors={["#FFFFFF"]}
-                                        />}
-                        leftOpenValue={75}
-                        rightOpenValue={-75}
-                        dataSource={this.ds.cloneWithRows(this.state.skills)}
-                        renderRow={skill =>
-                            <ListItem style={{ backgroundColor: '#131726', justifyContent: 'center' }}>
-                                <Text style={commonStyles.menuText}> {skill.name} </Text>
-                            </ListItem>}
-                        renderLeftHiddenRow={(skill, secId, rowId, rowMap) =>
-                            <View >
-                                {skill.userKnows?
-                                    <Button 
-                                        full danger 
-                                        style={{ height: "100%" }}
-                                        onPress={_ => this.removeSkillPressed(secId, rowId, rowMap)}>
-                                        <Icon active name="md-remove" />
-                                    </Button>
-                                    :
-                                    <Button 
-                                        full success
-                                        style={{ height: "100%" }}
-                                        onPress={_ => this.addSkillPressed(secId, rowId, rowMap)}>
-                                        <Icon active name="md-add" />
-                                    </Button>
-                                }
-                            </View>
-                        }
-                        renderRightHiddenRow={(skill, secId, rowId, rowMap) =>
-                            <Button full danger onPress={_ => this.deleteRow(secId, rowId, rowMap)}>
-                                <Icon active name="trash" />
-                            </Button>}
-                    />
-                    <Fab
-                        onPress={this.props.redirectToCreate}
-                        style={{ backgroundColor: '#5067FF' }}>
-                        <Icon name="md-add" />
-                    </Fab>
-                </View>
-        );
-    }
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <Text style={commonStyles.errorTextStyle}>{this.state.error}</Text>
+        <List
+          refreshControl={
+            <RefreshControl
+              refreshing={this.state.loading}
+              onRefresh={this._onRefresh}
+              progressBackgroundColor={"#5cb85c"}
+              colors={["#FFFFFF"]}
+            />
+          }
+          leftOpenValue={75}
+          rightOpenValue={-75}
+          dataSource={this.ds.cloneWithRows(this.state.skills)}
+          renderRow={skill => (
+            <ListItem
+              style={{ backgroundColor: "#131726", justifyContent: "center" }}
+            >
+              <Text style={commonStyles.menuText}> {skill.name} </Text>
+            </ListItem>
+          )}
+          renderLeftHiddenRow={(skill, secId, rowId, rowMap) => (
+            <View>
+              {skill.userKnows ? (
+                <Button
+                  full
+                  danger
+                  style={{ height: "100%" }}
+                  onPress={_ => this.removeSkillPressed(secId, rowId, rowMap)}
+                >
+                  <Icon active name="md-remove" />
+                </Button>
+              ) : (
+                <Button
+                  full
+                  success
+                  style={{ height: "100%" }}
+                  onPress={_ => this.addSkillPressed(secId, rowId, rowMap)}
+                >
+                  <Icon active name="md-add" />
+                </Button>
+              )}
+            </View>
+          )}
+          renderRightHiddenRow={(skill, secId, rowId, rowMap) => (
+            <Button
+              full
+              danger
+              onPress={_ => this.deleteRow(secId, rowId, rowMap)}
+            >
+              <Icon active name="trash" />
+            </Button>
+          )}
+        />
+        <Fab
+          onPress={this.props.redirectToCreate}
+          style={{ backgroundColor: "#5067FF" }}
+        >
+          <Icon name="md-add" />
+        </Fab>
+      </View>
+    );
+  }
 }
